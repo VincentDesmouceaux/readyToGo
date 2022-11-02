c895b2cca92782888102954962dd6ece23f3a072
vincent desmouceaux <vincentdesmont@mbp-de-vincent.lan>
2022-11-02 17:19:21 +0100

    colors1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="on">
+        <div className="onoff">
+          <button
+            style={{
+              background: active ? "white" : "purple",
+              textColor: "white",
+            }}
+          >
+            ON
+          </button>
+          <button>OFF</button>
+        </div>
+        <div className="onoff">
+          <button>ON</button>
+          <button>OFF</button>
+        </div>
+        <div className="onoff">
+          <button>ON</button>
+          <button>OFF</button>
+        </div>
+      </div>
+      <div className="noway">
+        <button>No Way</button>
+      </div>
     </div>
   );
 }
